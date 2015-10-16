570c893feaba1d3d75675baee4c96ecd72a25763
Andreas Kohn <andreas.kohn@gmail.com>
2015-10-16 15:02:23 +0200

    Update de.flapdoodle.embed.mongo to 1.50.0

--- src/test/java/org/elasticsearch/river/mongodb/embed/TokuArtifactStoreBuilder.java
+++ src/test/java/org/elasticsearch/river/mongodb/embed/TokuArtifactStoreBuilder.java
@@ -1,9 +1,6 @@
 package org.elasticsearch.river.mongodb.embed;
 
 import de.flapdoodle.embed.mongo.Command;
-import de.flapdoodle.embed.process.config.store.ILibraryStore;
-import de.flapdoodle.embed.process.config.store.PlatformLibraryStoreBuilder;
-import de.flapdoodle.embed.process.distribution.Platform;
 import de.flapdoodle.embed.process.extract.UUIDTempNaming;
 import de.flapdoodle.embed.process.io.directories.PropertyOrPlatformTempDir;
 
@@ -14,13 +11,6 @@ public class TokuArtifactStoreBuilder extends de.flapdoodle.embed.process.store.
         executableNaming().setDefault(new UUIDTempNaming());
         download().setDefault(new TokuDownloadConfigBuilder().defaultsForCommand(command).build());
         downloader().setDefault(new TokuDownloader());
-        libraries().setDefault(libraryStore());
         return this;
-    }
-
-    private ILibraryStore libraryStore() {
-        PlatformLibraryStoreBuilder libraryStoreBuilder = new PlatformLibraryStoreBuilder().defaults();
-        libraryStoreBuilder.setLibraries(Platform.Linux, new String[] { "libHotBackup.so" });
-        return libraryStoreBuilder.build();
     }
 }
